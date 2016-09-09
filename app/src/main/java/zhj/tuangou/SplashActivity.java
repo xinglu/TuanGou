c24f6de7c61ba1775ca5067a4348ca591081ff55
HongJay <zjxuzhj@gmail.com>
2016-09-09 11:21:59 +0800

    Initial commit

---
+++ app/src/main/java/zhj/tuangou/SplashActivity.java
@@ -0,0 +1,35 @@
+package zhj.tuangou;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+    Handler mHandler = new Handler();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        initViews();
+
+        //延迟两秒进入
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent();
+                intent.setClass(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        },2000);
+    }
+
+
+
+    private void initViews() {
+    }
+}
